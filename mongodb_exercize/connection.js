9791095cb066766faf47033f547bb247c681f985
xav1002 <orchdork.2002@gmail.com>
2018-11-05 09:51:09 -0600

    downloading mongodb and mongoose

--- mongodb_exercize/connection.js
+++ mongodb_exercize/connection.js
@@ -3,3 +3,8 @@ const mongoose = require('mongoose');
 // Connect to mongodb
 mongoose.connect('mongodb://localhost/example');
 
+mongoose.connection.once('open', function() {
+    console.log('Connection has been made');
+}).on('error', function(error) {
+    console.log('Connection error:', error);
+});
