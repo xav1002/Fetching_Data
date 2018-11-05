f325da7a9d3e4b163d8644255a06ec2021bfc772
xav1002 <orchdork.2002@gmail.com>
2018-11-05 11:19:44 -0600

    connecting to mongodb

---
+++ example/connection.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+// Connect to mongodb
+mongoose.connect('mongodb://localhost/example');
+
+mongoose.connection.once('open', function() {
+    console.log("Connection has been made");
+}).on('error', function(error) {
+    console.log("Connection error:", error);
+});
