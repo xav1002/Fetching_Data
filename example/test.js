b22a85263acce3e64800ef1c0c85d7e85e2ba2fd
xav1002 <orchdork.2002@gmail.com>
2018-11-11 19:13:36 -0600

    mocha example

---
+++ example/test.js
@@ -0,0 +1,10 @@
+const assert = require('assert');
+
+describe('demo test', function() {
+
+    // Create tests
+    it('adds two numbers together', function() {
+        assert(2 + 3 === 5);
+    })
+
+});
